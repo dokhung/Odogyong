ce5095ebc2a30954d8ab7948703c7d915f2ad8fe
dokhung <94828988+dokhung@users.noreply.github.com>
2024-09-12 15:06:33 +0900

    fork?

---
+++ Odokhung/notice_study/src/main/java/com/ohgiraffers/Main.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 공지사항 구현
+
+        // 메뉴 리스트 표현
+        System.out.println("목록 보기");
+        while(true){
+            // 1. 공지사항 목록보기
+            // 2. 공지사항 등록 기능
+            // 3. 공지사항 수정 기능
+            // 4. 공지사항 삭제 기능
+        }
+    }
+
+}
